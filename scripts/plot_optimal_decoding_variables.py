57a0c050e7df2a6bc53689d20fd78289c26c1cc3
nk479 <nirag.kadakia@yale.edu>
2017-08-25 09:39:58 -0400

    Tiny change

--- scripts/plot_optimal_decoding_variables.py
+++ scripts/plot_optimal_decoding_variables.py
@@ -64,4 +64,4 @@ def plot_optimal_decoding_variables(data_flag, axes_to_plot=[0, 1]):
 	
 if __name__ == '__main__':
 	data_flag = get_flag()
-	plot_optimal_decoding_variables(data_flag, axes_to_plot=[1,0])
+	plot_optimal_decoding_variables(data_flag)
