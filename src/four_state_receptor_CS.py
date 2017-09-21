cb85b02f3c27abc9e0ba24689bf7609f39bbb69b
nk479 <nirag.kadakia@yale.edu>
2017-09-21 12:19:39 -0400

    Clean up four-state-receptor

--- src/four_state_receptor_CS.py
+++ src/four_state_receptor_CS.py
@@ -64,49 +64,60 @@ class four_state_receptor_CS:
 		self.mu_Ss0 = 1.
 		self.sigma_Ss0 = 0.001
 		
-		# K1
+		# All K1 and K2 from a single Gaussian distribution
 		self.mu_Kk1 = 1e4
 		self.sigma_Kk1 = 1e3
-		self.mu_Kk1_lo = 1e4
-		self.mu_Kk1_hi = 1e4
-		self.sigma_Kk1_lo = 1e3
-		self.sigma_Kk1_hi = 1e3
-		self.uniform_Kk1_lo = 1e2
-		self.uniform_Kk1_hi = 1e3
+		self.mu_Kk2 = 1e-3
+		self.sigma_Kk2 = 1e-4
 		
-		# K2
-		self.mu_Kk2_lo = 1e-3
-		self.mu_Kk2_hi = 1e-3
-		self.sigma_Kk2_lo = 1e-4
-		self.sigma_Kk2_hi = 1e-4
-		self.uniform_Kk2_lo = 1e-3
-		self.uniform_Kk2_hi = 1e-2
-		
-		# K1-K2 mixture	
+		# K2: each receptor from one of two Gaussians; these 
+		# are the stats of the second Gaussian.
 		self.mu_Kk2_2 = 1e-3
 		self.sigma_Kk2_2 = 1e-4
 		self.Kk2_p = 0.5
 		
-		# normal activity mixture
+		# All K1 and K2 from a single uniform distribution
+		self.uniform_Kk1_lo = 1e2
+		self.uniform_Kk1_hi = 1e3
+		self.uniform_Kk2_lo = 1e-3
+		self.uniform_Kk2_hi = 1e-2
+		
+		# K1 and K2: each receptor from a distinct Gaussian, 
+		# with uniform prior on means and sigmas
+		self.mu_Kk1_hyper_lo = 1e4
+		self.mu_Kk1_hyper_hi = 1e4
+		self.sigma_Kk1_hyper_lo = 1e3
+		self.sigma_Kk1_hyper_hi = 1e3
+		self.mu_Kk2_hyper_lo = 1e-3
+		self.mu_Kk2_hyper_hi = 1e-3
+		self.sigma_Kk2_hyper_lo = 1e-4
+		self.sigma_Kk2_hyper_hi = 1e-4
+				
+		# Fixed activity distributions for adapted individual odorant response, 
+		# where each activity is chosen from mixture of 2 Gaussians
 		self.activity_p = 0.5
 		self.receptor_tuning_mixture_mu_1 = 0.5
 		self.receptor_tuning_mixture_mu_2 = 0.5
 		self.receptor_tuning_mixture_sigma_1 = 0.1
 		self.receptor_tuning_mixture_sigma_2 = 0.1
 		
-		# uniform activity
+		# Fixed activity distributions for adapted individual odorant response, 
+		# where each activity is chosen from a single uniform distribution
 		self.uniform_activity_lo = 1e-5
 		self.uniform_activity_hi = 1e-4
  		
+		# Fixed activity distributions for adapted individual odorant response, 
+		# where each activity is chosen from a normal distribution depending on
+		# the receptor. The means for each receptor are chosen normally and the 
+		# sigmas are chosen uniformly. 
+		self.receptor_tuning_mu_hyper_mu = 0.2
+		self.receptor_tuning_mu_hyper_sigma = 0
+		self.receptor_tuning_sigma_hyper_lo = 0
+		self.receptor_tuning_sigma_hyper_hi = 0.3
+		
 		# Free energy statistics
 		self.mu_eps = 5.0
 		self.sigma_eps = 0.0
-		
-		# Fixed tuning curve statistics for adapted individual activity. 
-		self.receptor_tuning_center_mean = 0.2
-		self.receptor_tuning_center_dev = 0
-		self.receptor_tuning_range_lo = 0
-		self.receptor_tuning_range_hi = 0.3
 		
 		# Fix tuning curve statistics for adapted full activity
 		self.adapted_activity_mu = 0.5
@@ -139,19 +150,27 @@ class four_state_receptor_CS:
 		self.Ss = self.dSs + self.Ss0_noisy
 	
 	def set_adapted_free_energy(self):
-		# Set free energy based on adapted activity activity
+		"""Set free energy based on adapted activity activity."""
+		
 		activity_stats = [self.adapted_activity_mu, self.adapted_activity_sigma]
 		adapted_activity = random_matrix([self.Mm], params=activity_stats, 
 									seed=self.seed_adapted_activity)
 		self.eps = free_energy(self.Ss0, self.Kk1, self.Kk2, adapted_activity)
 		
 	def set_random_free_energy(self):
-		# Free energy as random vector if assigned as such		
+		"""Set free energy as random vector."""
+		
 		self.eps = random_matrix([self.Mm], [self.mu_eps, self.sigma_eps], 
 									seed = self.seed_eps)
 
 	def set_normal_Kk_Kk2_mixture(self):
-		# Define Kk matrices as being chosen from a Gaussian mixture
+		"""
+		Set K1 and K2 matrices, where K1 is chosen from normal distributions 
+		with hyperdistributions for each mean and sigma (normal and uniform; 
+		mu_Kk1, sigma_Kk1) and for K2, each receptor response is chosen from 
+		a Gaussian mixture with stats mu_Kk1, sigma_Kk2, mu_Kk1_2, sigma_Kk2_2.
+		"""
+		
 		params_Kk1 = [self.mu_Kk1, self.sigma_Kk1]
 		self.Kk1 = random_matrix([self.Mm, self.Nn], params_Kk1, 
 									seed = self.seed_Kk1)
@@ -160,36 +179,42 @@ class four_state_receptor_CS:
 		
 		num_comp1 = int(self.Kk2_p*self.Mm)
 		num_comp2 = self.Mm - num_comp1
-		self.Kk2 = sp.zeros(self.Kk1.shape)
 		params_Kk2_1 = [self.mu_Kk2, self.sigma_Kk2]
+		params_Kk2_2 = [self.mu_Kk2_2, self.sigma_Kk2_2]
 		
+		self.Kk2 = sp.zeros(self.Kk1.shape)
 		self.Kk2[:num_comp1, :] = random_matrix([num_comp1, self.Nn], 
 										params_Kk2_1, seed = self.seed_Kk2)		
-		params_Kk2_2 = [self.mu_Kk2_2, self.sigma_Kk2_2]
 		self.Kk2[num_comp1:, :] = random_matrix([num_comp2, self.Nn], 
 										params_Kk2_2, seed = self.seed_Kk2)
 								
 									
 	def set_normal_Kk(self, clip=True):	
-		# Define class object numpy array of Kk1 and Kk2 matrix, given 
-		# prescribed Gaussian statistics.
-		Kk1_mus = random_matrix([self.Mm], params=[self.mu_Kk1_lo, 
-								self.mu_Kk1_hi], sample_type='uniform',
-								seed=self.seed_Kk1)
-		Kk1_sigmas = random_matrix([self.Mm], params=[self.sigma_Kk1_lo, 
-									self.sigma_Kk1_hi], sample_type='uniform',
-									seed=self.seed_Kk1)
-		Kk2_mus = random_matrix([self.Mm], params=[self.mu_Kk2_lo, 
-								self.mu_Kk2_hi], sample_type='uniform',
-								seed=self.seed_Kk2)
-		Kk2_sigmas = random_matrix([self.Mm], params=[self.sigma_Kk2_lo, 
-									self.sigma_Kk2_hi], sample_type='uniform',
-									seed=self.seed_Kk2)
+		"""
+		Set K1 and K2 where each receptor from a distinct Gaussian with 
+		uniform prior on means and sigmas (mu_Kk1_hyper_lo, mu_Kk1_hyper_hi, 
+		sigma_Kk1_hyper_lo, sigma_Kk1_hyper_hi, etc.)
+		"""
+		
+		Kk1_mus = random_matrix([self.Mm], params=[self.mu_Kk1_hyper_lo, 
+							self.mu_Kk1_hyper_hi], sample_type='uniform',
+							seed=self.seed_Kk1)
+		Kk1_sigmas = random_matrix([self.Mm], params=[self.sigma_Kk1_hyper_lo, 
+							self.sigma_Kk1_hyper_hi], sample_type='uniform',
+							seed=self.seed_Kk1)
+		Kk2_mus = random_matrix([self.Mm], params=[self.mu_Kk2_hyper_lo, 
+							self.mu_Kk2_hyper_hi], sample_type='uniform',
+							seed=self.seed_Kk2)
+		Kk2_sigmas = random_matrix([self.Mm], params=[self.sigma_Kk2_hyper_lo,
+							self.sigma_Kk2_hyper_hi], sample_type='uniform',
+							seed=self.seed_Kk2)
 		
 		self.Kk1 = random_matrix([self.Mm, self.Nn], [Kk1_mus, Kk1_sigmas], 
-									sample_type='rank2_row_gaussian', seed = self.seed_Kk1)
+									sample_type='rank2_row_gaussian', 
+									seed = self.seed_Kk1)
 		self.Kk2 = random_matrix([self.Mm, self.Nn], [Kk2_mus, Kk2_sigmas],
-									sample_type='rank2_row_gaussian', seed = self.seed_Kk2)
+									sample_type='rank2_row_gaussian', 
+									seed = self.seed_Kk2)
 		
 
 		if clip == True:
@@ -198,48 +223,52 @@ class four_state_receptor_CS:
 			self.Kk2 = array_dict['Kk2']
 	
 	def set_uniform_Kk(self, clip=True):	
-		# Define class object numpy array of Kk1 and Kk2 matrix, given 
-		# prescribed uniform statistics.
+		"""
+		K1 and K2 are chosen from a uniform distribution.
+		"""
+		
 		self.Kk1 = random_matrix([self.Mm, self.Nn], [self.uniform_Kk1_lo, 
-									self.uniform_Kk1_hi], sample_type='uniform', 
-									seed = self.seed_Kk1)
+								self.uniform_Kk1_hi], sample_type='uniform',
+								seed = self.seed_Kk1)
 		self.Kk2 = random_matrix([self.Mm, self.Nn], [self.uniform_Kk2_lo, 
-									self.uniform_Kk2_hi], sample_type='uniform', 
-									seed = self.seed_Kk2)
-			
+								self.uniform_Kk2_hi], sample_type='uniform', 
+								seed = self.seed_Kk2)
+		
 	def set_Kk2_normal_activity(self, clip=True, **kwargs):
-		# Define numpy array of Kk2 matrix, given prescribed monomolecular 
-		# tuning curve statistics, and Kk1 matrix from a Gaussian prior.
-		# kwargs allow overriding parameters for user-defined matrix as well.
+		"""
+		Fixed activity distributions for adapted individual odorant response, 
+		where each activity is chosen from a normal distribution depending on
+		the receptor. The means for each receptor are chosen normally and the 
+		sigmas are chosen uniformly. Kk2 is then derived from these choices,
+		while Kk1 is chosen from a single Gaussian distribution. The function
+		allows for overriding of mu_eps and mu_Ss0 if system has adapted to 
+		another background state.
+		"""
 		
 		matrix_shape = [self.Mm, self.Nn]
 		
 		params_Kk1 = [self.mu_Kk1, self.sigma_Kk1]
 		self.Kk1 = random_matrix(matrix_shape, params_Kk1, seed=self.seed_Kk1)
 	
-		center_mean = self.receptor_tuning_center_mean
-		center_dev = self.receptor_tuning_center_dev
-		range_lo = self.receptor_tuning_range_lo
-		range_hi = self.receptor_tuning_range_hi
 		mu_Ss0 = self.mu_Ss0
 		mu_eps = self.mu_eps
-		seed_Kk2 = self.seed_Kk2
-		
 		for key in kwargs:
 			exec ('%s = kwargs[key]' % key)
 		
-		center_stats = [center_mean, center_dev]
-		range_stats = [range_lo, range_hi]
-		activity_mus = random_matrix([self.Mm], params=center_stats, 
+		mu_stats = [self.receptor_tuning_mu_hyper_mu, 
+					self.receptor_tuning_mu_hyper_sigma]
+		sigma_stats = [self.receptor_tuning_sigma_hyper_lo, 
+						self.receptor_tuning_sigma_hyper_hi]
+		activity_mus = random_matrix([self.Mm], params=mu_stats, 
 										sample_type='normal',
 										seed=self.seed_receptor_activity)
-		activity_sigmas = random_matrix([self.Mm], params=range_stats, 
+		activity_sigmas = random_matrix([self.Mm], params=sigma_stats, 
 										sample_type='uniform',
 										seed=self.seed_receptor_activity)
 		
 		self.Kk2 = Kk2_eval_normal_activity(matrix_shape, activity_mus, 
-											activity_sigmas, mu_Ss0, mu_eps, 
-											seed_Kk2)
+										activity_sigmas, mu_Ss0, mu_eps, 
+										self.seed_Kk2)
 		
 		if clip == True:
 			array_dict = clip_array(dict(Kk1 = self.Kk1, Kk2 = self.Kk2))
@@ -247,7 +276,10 @@ class four_state_receptor_CS:
 			self.Kk2 = array_dict['Kk2']
 	
 	def set_Kk2_uniform_activity(self, clip=True, **kwargs):
-		# TODO
+		"""
+		Fixed activity distributions for adapted individual odorant response, 
+		where each activity is chosen from a single uniform distribution.
+		"""
 		
 		matrix_shape = [self.Mm, self.Nn]
 		
@@ -255,8 +287,9 @@ class four_state_receptor_CS:
 		self.Kk1 = random_matrix(matrix_shape, params_Kk1, seed=self.seed_Kk1)
 	
 		params_Kk2 = [self.uniform_activity_lo, self.uniform_activity_hi]
-		self.Kk2 = Kk2_eval_uniform_activity(matrix_shape, params_Kk2, self.mu_Ss0, 
-												self.mu_eps, self.seed_Kk2)
+		self.Kk2 = Kk2_eval_uniform_activity(matrix_shape, params_Kk2, 
+											self.mu_Ss0, self.mu_eps, 
+											self.seed_Kk2)
 		
 		if clip == True:
 			array_dict = clip_array(dict(Kk1 = self.Kk1, Kk2 = self.Kk2))
@@ -264,66 +297,43 @@ class four_state_receptor_CS:
 			self.Kk2 = array_dict['Kk2']
 	
 	def set_Kk2_normal_activity_mixture(self, clip=True, **kwargs):
-		# TODO
+		"""
+		Fixed activity distributions for adapted individual odorant response, 
+		where each activity is chosen from a Gaussian mixture.
+		"""
 		
 		matrix_shape = [self.Mm, self.Nn]
 		
 		params_Kk1 = [self.mu_Kk1, self.sigma_Kk1]
 		self.Kk1 = random_matrix(matrix_shape, params_Kk1, seed=self.seed_Kk1)
 	
-		mu_1 = self.receptor_tuning_mixture_mu_1
-		mu_2 = self.receptor_tuning_mixture_mu_2
-		sigma_1 = self.receptor_tuning_mixture_sigma_1
-		sigma_2 = self.receptor_tuning_mixture_sigma_2
 		mu_Ss0 = self.mu_Ss0
 		mu_eps = self.mu_eps
-		seed_Kk2 = self.seed_Kk2
-		
 		for key in kwargs:
 			exec ('%s = kwargs[key]' % key)
 		
 		assert 0 <= self.activity_p <= 1., "Mixture ratio must be between 0 and 1"
 		
+		activity_mus = sp.zeros(self.Mm)
+		activity_sigmas = sp.zeros(self.Mm)
+		
 		num_comp1 = int(self.activity_p*self.Mm)
 		num_comp2 = self.Mm - num_comp1
 		
-		activity_mus = sp.zeros(self.Mm)
-		activity_sigmas = sp.zeros(self.Mm)
-		self.Kk2 = sp.zeros(self.Kk1.shape)
-		
-		activity_mus[:num_comp1] = mu_1
-		activity_mus[num_comp1:] = mu_2
-		activity_sigmas[:num_comp1] = sigma_1
-		activity_sigmas[num_comp1:] = sigma_2
+		activity_mus[:num_comp1] = self.receptor_tuning_mixture_mu_1
+		activity_mus[num_comp1:] = self.receptor_tuning_mixture_mu_2
+		activity_sigmas[:num_comp1] = self.receptor_tuning_mixture_sigma_1
+		activity_sigmas[num_comp1:] = self.receptor_tuning_mixture_sigma_2
 		
 		self.Kk2 = Kk2_eval_normal_activity(matrix_shape, activity_mus, 
 											activity_sigmas, mu_Ss0, mu_eps, 
-											seed_Kk2)
+											self.seed_Kk2)
 		
 		if clip == True:
 			array_dict = clip_array(dict(Kk1 = self.Kk1, Kk2 = self.Kk2))
 			self.Kk1 = array_dict['Kk1']
 			self.Kk2 = array_dict['Kk2']
 	
-	
-	def set_Kk2_exponential_activity(self):
-		# Define numpy array of Kk2 matrix, given prescribed monomolecular 
-		# tuning curve statistics, and Kk1 matrix from a Gaussian prior.
-		params_Kk1 = [self.mu_Kk1, self.sigma_Kk1]
-		receptor_tuning_center = [self.receptor_tuning_center_mean, 
-										self.receptor_tuning_center_dev]
-		
-		receptor_activity_mus = random_matrix([self.Mm], 
-										params=receptor_tuning_center,
-										sample_type='normal', 
-										seed = self.seed_receptor_activity)
-		
-		self.Kk2 = Kk2_eval_exponential_activity([self.Mm, self.Nn], 
-										receptor_activity_mus, self.mu_Ss0,
-										self.mu_eps, self.seed_Kk2)
-		
-		self.Kk1 = random_matrix([self.Mm, self.Nn], params_Kk1, 
-									seed = self.seed_Kk1)
 	
 	def set_measured_activity(self):
 		# True receptor activity
@@ -341,8 +351,7 @@ class four_state_receptor_CS:
 		self.Rr = linear_gain(self.Ss0, self.Kk1, self.Kk2, self.eps)
 			
 	def encode_normal_activity(self, **kwargs):
-		# Run all functions to encode the response when the tuning curves
-		# are assumed normal, and Kk matrices generated thereof.
+		# Run all functions to encode when activity is normally distributed.
 		self.set_signals()
 		self.set_random_free_energy()
 		self.set_Kk2_normal_activity(**kwargs)
@@ -350,7 +359,7 @@ class four_state_receptor_CS:
 		self.set_linearized_response()
 	
 	def encode_uniform_activity(self, **kwargs):
-		# TODO
+		# Run all functions to encode when activity is uniformly distributed.
 		self.set_signals()
 		self.set_random_free_energy()
 		self.set_Kk2_uniform_activity()
@@ -358,25 +367,15 @@ class four_state_receptor_CS:
 		self.set_linearized_response()
 	
 	def encode_normal_activity_mixture(self, **kwargs):
-		# TODO
+		# Run all functions to encode when activity arises from a mixture.
 		self.set_signals()
 		self.set_random_free_energy()
 		self.set_Kk2_normal_activity_mixture(**kwargs)
 		self.set_measured_activity()
 		self.set_linearized_response()
 	
-	def encode_exponential_activity(self):
-		# Run all functions to encode the response when the tuning curves
-		# are assumed normal, and Kk matrices generated thereof.
-		self.set_signals()
-		self.set_random_free_energy()
-		self.set_Kk2_exponential_activity()
-		self.set_measured_activity()
-		self.set_linearized_response()
-	
 	def encode_normal_Kk(self):
-		# Run all functions to encode the response when the Kk matrices
-		# are assumed normal.
+		# Run all functions to encode when K matrices are Gaussian.
 		self.set_signals()
 		self.set_random_free_energy()
 		self.set_normal_Kk()
@@ -384,8 +383,7 @@ class four_state_receptor_CS:
 		self.set_linearized_response()
 	
 	def encode_uniform_Kk(self):
-		# Run all functions to encode the response when the Kk matrices
-		# are assumed uniformly distributed.
+		# Run all functions to encode when K matrices are uniform.
 		self.set_signals()
 		self.set_random_free_energy()
 		self.set_uniform_Kk()
@@ -393,7 +391,8 @@ class four_state_receptor_CS:
 		self.set_linearized_response()
 	
 	def encode_adapted_normal_activity(self):
-		# Assume entire activity has adapted, not activity to single molecules.
+		# Run all functions to encode when full activity of each receptor 
+		# is normal.
 		self.set_signals()
 		self.set_normal_Kk()
 		self.set_adapted_free_energy()
@@ -401,7 +400,8 @@ class four_state_receptor_CS:
 		self.set_linearized_response()
 
 	def encode_adapted_normal_activity_Kk2_mixture(self):
-		# Assume entire activity has adapted, not activity to single molecules.
+		# Run all functions to encode when full activity of each receptor 
+		# is from a Gaussian mixture.
 		self.set_signals()
 		self.set_normal_Kk_Kk2_mixture()
 		self.set_adapted_free_energy()
@@ -413,4 +413,3 @@ class four_state_receptor_CS:
 		
 	def decode_nonlinear(self):
 		self.dSs_est = decode_nonlinear_CS(self)
-		
