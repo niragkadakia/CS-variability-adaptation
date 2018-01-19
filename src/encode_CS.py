bbbeb0ed554a71baf0f058601ed4c0550a3be3a4
nk479 <nirag.kadakia@yale.edu>
2018-01-19 09:41:52 -0500

    Part of last commit

--- src/encode_CS.py
+++ src/encode_CS.py
@@ -10,20 +10,20 @@ To view a copy of this license,
 visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
 """
 
-from four_state_receptor_CS import four_state_receptor_CS
 
-
-def single_encode_CS(vars_to_pass=dict(), run_specs=dict()):
+def single_encode_CS(obj, run_specs=dict()):
 	"""
 	Run CS encoding and decoding via four_state_receptor_CS; single iteration.
+	Reads run_specs and performs appropriate encoding scheme depending on 
+	specs input.
 	
-	Optional args:
-		vars_to_pass: dictionary; any overriden arguments to 
-						four_state_receptor_CS.
+	Args:
+		obj: four_state_receptor_CS.py class object 
 		run_specs: dictionary; parameters of the run.
+		
+	Returns:
+		obj: now with variables updated via encoding.
 	"""		
-	
-	a = four_state_receptor_CS(**vars_to_pass)
 	
 	if 'run_type' in run_specs.keys():
 		val = run_specs['run_type']
@@ -31,21 +31,21 @@ def single_encode_CS(vars_to_pass=dict(), run_specs=dict()):
 			override_parameters = dict()
 			override_parameters['mu_Ss0'] = float(val[1])
 			override_parameters['mu_eps'] = float(val[2])
-			a.encode_normal_activity(**override_parameters)
+			obj.encode_normal_activity(**override_parameters)
 		elif val[0] == 'uniform_activity_fixed_Kk2':
 			override_parameters = dict()
 			override_parameters['mu_Ss0'] = float(val[1])
 			override_parameters['mu_eps'] = float(val[2])
-			a.encode_uniform_activity(**override_parameters)		
+			obj.encode_uniform_activity(**override_parameters)		
 		else:
 			try:
-				str = 'a.encode_%s()' % val[0]
+				str = 'obj.encode_%s()' % val[0]
 				exec(str)
 			except AttributeError:
 				print ('Run specification %s not recognized' % val[0])
 				quit()
 	else:
 		print ('No run type specified, proceeding with normal_activity')
-		a.encode_normal_activity()
+		obj.encode_normal_activity()
 	
-	return a
+	return obj
