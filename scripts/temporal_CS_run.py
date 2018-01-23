4dfe38e093fb15525d31d362722978bb130bed38
nk479 <nirag.kadakia@yale.edu>
2018-01-22 20:13:57 -0500

    temporal coding -- save periodically only

--- scripts/temporal_CS_run.py
+++ scripts/temporal_CS_run.py
@@ -97,7 +97,9 @@ def temporal_CS_run(data_flag, iter_var_idxs, sigma_Ss0=0,
 		# Deep copy to take all aspects of the object but not update it
 		obj_list.append(copy.deepcopy(obj))
 		
-		dump_objects(obj_list, iter_var_idxs, data_flag, output=False)
+		# Save periodically
+		if iT % 200 == 0:
+			dump_objects(obj_list, iter_var_idxs, data_flag)
 	
 	return obj_list
 	
